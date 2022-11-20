d9e6ee1852c8a31ca026987f4a3c325ec56f5b3a
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-20 20:55:48 +0300

    https://www.patika.dev/tr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
+    static void primeNumber(int num){
+        if (num==2||num==3){
+
+            System.out.println("number =prime number");
+
+        }
+        if ((num%2!=0 && num%3!=0)&&(num%5!=0 && num%7!=0)){
+            System.out.println("number =prime number");
+
+        }
+        else{
+            System.out.println("not prime number");
+
+        }
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter number");
+        int num=scanner.nextInt();
+        primeNumber(num);
+
+
+
     }
 }
